f91a887464b393144b1c221b8c2aecb12b2e7005
Lokesh Bisht <lokeshbisht.096@gmail.com>
2023-10-24 23:35:26 +0530

    Bottom view of binary tree problem

---
+++ gfg/Tree/Traversal/BottomViewOfBinaryTree.cpp
@@ -0,0 +1,58 @@
+/**
+ * Copyright (C) 2023 Lokesh Bisht
+ * 
+ * Problem Link: https://practice.geeksforgeeks.org/problems/bottom-view-of-binary-tree/1?page=1&category[]=Tree&sortBy=submissions
+ * 
+ * TC => O(n)
+ * SC => O(n)
+*/
+
+#include <cstdio>
+#include <queue>
+#include <vector>
+#include <utility>
+#include <unordered_map>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left, *right;
+
+    Node(int data) {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+vector<int> bottomView(Node* root) {
+    vector<int> ans;
+    if (!root) {
+        return ans;
+    }
+    queue<pair<Node*, int>> queue;
+    queue.push({root, 0});
+    unordered_map<int, int> hashMap;
+    int minDist = INT_MAX, maxDist = INT_MIN;
+    while (!queue.empty()) {
+        int n = queue.size();
+        for (int i = 0; i < n; ++i) {
+            Node* cur = queue.front().first;
+            int dist = queue.front().second;
+            queue.pop();
+            minDist = min(minDist, dist);
+            maxDist = max(maxDist, dist);
+            hashMap[dist] = cur->data;
+            if (cur->left) {
+                queue.push({cur->left, dist - 1});
+            }
+            if (cur->right) {
+                queue.push({cur->right, dist + 1});
+            }
+        }
+    }
+    for (int i = minDist; i <= maxDist; ++i) {
+        ans.push_back(hashMap[i]);
+    }
+    return ans;
+}
